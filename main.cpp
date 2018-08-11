4782b2ef83aa11006c994e304806052a0bb1b9ea
end222 <pabloordua98@gmail.com>
2018-08-12 00:41:56 +0200

    Minimal functionality

--- main.cpp
+++ main.cpp
@@ -6,18 +6,74 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <memory>
+#include <array>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 using namespace std;
+
+string exec(const char* cmd) {
+	array<char, 128> buffer;
+	string result;
+	shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);	
+	if (!pipe) throw runtime_error("popen() failed!");
+		while (!feof(pipe.get())) {
+			if (fgets(buffer.data(), 128, pipe.get()) != nullptr)
+				result += buffer.data();
+		}
+	return result;
+}
 
 void command_loop()
 {
 	bool exit = false;
 	char line[256];
+	char bin[20] = "/bin";
+	char *path[10] = {bin};
+	char *args[10];
+
+	string system_name = exec("hostname");
+	string user_name = exec("whoami");
+	system_name.erase(system_name.length()-1); // Remove end of line from the string
+	user_name.erase(user_name.length()-1);
+
 	while (exit == false)
 	{
-		cout << "Name@Home > " << flush;
+		cout << user_name << " at " << system_name << " ~ " << flush;
 		cin.getline(line,256);
-		cout << line << endl;
+		if (!strcmp(line,"exit"))
+		{
+			exit = true;
+		}
+		else
+		{
+			int status;
+			pid_t pid, wait_pid;
+			if ((pid = fork()) == 0) // Child
+			{
+				int i = 0;
+				const char s[2] = " ";
+				char* token;
+				token = strtok(line, s);
+				while (token != NULL)
+				{
+					args[i] = token;
+					token = strtok(NULL, s);
+					i++;
+				}
+				execvpe(line,args,path);
+			}
+			else // Parent
+			{
+				do {
+					wait_pid = waitpid(pid, &status, WUNTRACED);
+				} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+			}
+		}
 	}
 }
 
